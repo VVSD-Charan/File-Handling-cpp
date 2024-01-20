050199b8531bfd3fc54a3c4fe8b9ca2a542c0a9b
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-01-20 17:05:27 +0530

    modified:   1_Writing-data-to-text-file/output/File3.txt 	modified:   1_Writing-data-to-text-file/output/writing-using-fstream.exe 	modified:   1_Writing-data-to-text-file/writing-using-fstream.cpp

--- 1_Writing-data-to-text-file/writing-using-fstream.cpp
+++ 1_Writing-data-to-text-file/writing-using-fstream.cpp
@@ -7,17 +7,24 @@ int main()
 {
     // Unlike ofstream , for fstream we must specify why file should be opened i.e ios::out for writing on file 
     fstream file("File2.txt",ios::out);
+    file << " Raasesaa ";
+    file.close();
+    // Closing file is mandatory
+
 
     // For multiple modes 
     fstream file2("File3.txt",ios::out | ios::app | ios ::in);
     // ios::out -> Opening the file for writing only 
     // ios::app -> For appending written data to existing data
     // ios::in  -> Opening the file for reading only
+    file2<<"Name1 ";
+    file2<<"Name2 ";
+    file2.flush();
+    // Flush is used to ensure that data has been pushed to the file 
+    file2.close();
 
-    file << " Raasesaa ";
 
-    file.close();
-
+   
     cout<<"Fstream data has been written\n";
 
     system("pause");
